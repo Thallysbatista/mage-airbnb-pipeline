62a9f53dcde6b591c39c1bdf26925b5bbb70ad1b
Thallys Batista <thallysbatista77@gmail.com>
2025-05-20 22:26:14 -0300

    Finaliza primeiro modelo dbt de staging

---
+++ dbt_airbnb_project/models/staging/stg_listings_and_reviews.sql
@@ -0,0 +1,39 @@
+{{ config(
+    materialized='table'
+) }}
+
+with source as (
+
+    select * 
+    from {{ source('raw_airbnb', 'listings_and_reviews_raw') }}
+
+),
+
+renamed as (
+
+    select
+        _id as listing_id,
+        name,
+        description,
+        property_type,
+        room_type,
+        bed_type,
+        minimum_nights,
+        maximum_nights,
+        cancellation_policy,
+        accommodates,
+        bedrooms,
+        beds,
+        number_of_reviews,
+        bathrooms,
+        price,
+        address.country as country,
+        address.market as market,
+        host.host_id as host_id,
+        host.host_name as host_name,
+        host.host_location as host_location
+    from source
+
+)
+
+select * from renamed
