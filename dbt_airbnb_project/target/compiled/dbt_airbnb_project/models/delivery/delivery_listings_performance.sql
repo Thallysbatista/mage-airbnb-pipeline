5d58d75218c0a19f4b8bd450b3f8b66c3b6d59a2
Thallys Batista <thallysbatista77@gmail.com>
2025-07-25 15:55:18 -0300

    alterando nome da coluna da tabela delivery_listings_performance

--- dbt_airbnb_project/target/compiled/dbt_airbnb_project/models/delivery/delivery_listings_performance.sql
+++ dbt_airbnb_project/target/compiled/dbt_airbnb_project/models/delivery/delivery_listings_performance.sql
@@ -19,7 +19,7 @@ select
   ,l.review_scores_rating            as avg_review_score
   ,coalesce(r.review_count, 0)       as total_reviews
   ,l.available_30                    as avail_next_30d
-  ,l.available_90                    as avail_next_90
+  ,l.available_90                    as avail_next_90d
 from listings l
 left join reviews r
   on l.listing_id = r.listing_id
